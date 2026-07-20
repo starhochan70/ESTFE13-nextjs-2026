707eebd98138fcc0aae22ad34012d5eb2d76f010
alikerock <alikerock@gmail.com>
2026-07-20 15:49:52 +0900

    Enhance Update component: fetch existing topic data and handle form submission

--- src/app/update/[id]/page.js
+++ src/app/update/[id]/page.js
@@ -1,14 +1,47 @@
 "use client";
-import { useRouter } from "next/navigation";
+import { useRouter, useParams } from "next/navigation";
+import { useState, useEffect } from "react";
 
 export default function Update() {
   console.log("Update Page 작동");
   const router = useRouter();
+  const [title, setTitle] = useState("");
+  const [message, setMessage] = useState("");
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch(`http://localhost:9999/topics/${id}`)
+      .then(res => res.json())
+      .then(result => {
+        setTitle(result.title);
+        setMessage(result.message);
+      });
+  }, [id]);
 
   return (
     <>
       <h3 style={styles.title}>Update Form</h3>
-      <form action="">
+      <form
+        action=""
+        onSubmit={e => {
+          e.preventDefault();
+          const options = {
+            method: "PATCH",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              title,
+              message,
+            }),
+          };
+          fetch(`http://localhost:9999/topics/${id}`, options)
+            .then(res => res.json())
+            .then(result => {
+              router.push(`/read/${result.id}`);
+            });
+        }}
+      >
         <div className="mb-3">
           <label htmlFor="title" className="form-label">
             title
@@ -18,6 +51,10 @@ export default function Update() {
             className="form-control"
             id="title"
             name="title"
+            value={title}
+            onChange={e => {
+              setTitle(e.target.value);
+            }}
             placeholder="글 제목을 입력해주세요"
           />
         </div>
@@ -25,7 +62,16 @@ export default function Update() {
           <label htmlFor="message" className="form-label">
             Message
           </label>
-          <textarea name="message" className="form-control" id="message" rows="3"></textarea>
+          <textarea
+            name="message"
+            value={message}
+            onChange={e => {
+              setMessage(e.target.value);
+            }}
+            className="form-control"
+            id="message"
+            rows="3"
+          ></textarea>
         </div>
         <button type="submit" className="btn btn-primary">
           입력
